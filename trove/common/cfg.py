jrodom/reddwarf

Initial commit with configurations support

reddwarf/common/cfg.py
@@ -144,6 +144,8 @@ common_opts = [
                help="Chunk size to stream to swift container."),
     cfg.IntOpt('backup_segment_max_size', default=2 * (1024 ** 3),
                help="Maximum size of each segment of the backup file."),
+    cfg.StrOpt('validation_rules',
+               default="/etc/reddwarf/validation-rules.json"),
 ]
 
 

reddwarf/guestagent/manager/mysql_service.py
@@ -34,6 +34,8 @@ FINAL_MYCNF = "/var/lib/mysql/my.cnf"
 TMP_MYCNF = "/tmp/my.cnf.tmp"
 DBAAS_MYCNF = "/etc/dbaas/my.cnf/my.cnf.%dM"
 MYSQL_BASE_DIR = "/var/lib/mysql"
+MYCNF_OVERRIDES = "/etc/mysql/conf.d/overrides.cnf"
+MYCNF_OVERRIDES_TMP = "/tmp/overrides.cnf.tmp"
 
 CONF = cfg.CONF
 INCLUDE_MARKER_OPERATORS = {
@@ -611,7 +613,7 @@ class MySqlApp(object):
     def complete_install_or_restart(self):
         self.status.end_install_or_restart()
 
-    def secure(self, memory_mb):
+    def secure(self, memory_mb, overrides):
         LOG.info(_("Generating admin password..."))
         admin_password = generate_random_password()
 
@@ -622,7 +624,7 @@ class MySqlApp(object):
             self._create_admin_user(client, admin_password)
 
         self.stop_db()
-        self._write_mycnf(memory_mb, admin_password)
+        self._write_mycnf(memory_mb, admin_password, overrides)
         self.start_mysql()
 
         LOG.info(_("Dbaas secure complete."))
@@ -754,7 +756,7 @@ class MySqlApp(object):
                 if "No such file or directory" not in str(pe):
                     raise
 
-    def _write_mycnf(self, update_memory_mb, admin_password):
+    def _write_mycnf(self, update_memory_mb, admin_password, overrides):
         """
         Install the set of mysql my.cnf templates from dbaas-mycnf package.
         The package generates a template suited for the current
@@ -789,6 +791,33 @@ class MySqlApp(object):
         LOG.info(_("Symlinking final my.cnf."))
         utils.execute_with_timeout("sudo", "ln", "-s", FINAL_MYCNF, ORIG_MYCNF)
         self.wipe_ib_logfiles()
+
+        # write configuration file overrides
+        if overrides:
+            self._write_config_overrides(overrides)
+
+    def _write_config_overrides(self, overrideValues):
+        LOG.info(_("Writing new temp overrides.cnf file."))
+        overrides = open(MYCNF_OVERRIDES_TMP, 'w')
+
+        # write the header of the overrides file
+        overrides.write("[mysqld]\n")
+
+        # overrides that have empty values should be expressed as-is and
+        # with no equals + value parameter.
+        for k, v in overrideValues.iteritems():
+            if not v:
+                overrides.write("%s\n" % k)
+            else:
+                overrides.write("%s = %s\n" % (k, v))
+
+        overrides.close()
+        LOG.info(_("Moving overrides.cnf into correct location."))
+        utils.execute_with_timeout("sudo", "mv", MYCNF_OVERRIDES_TMP,
+                                   MYCNF_OVERRIDES)
+
+        LOG.info(_("Setting permissions on overrides.cnf"))
+        utils.execute_with_timeout("sudo", "chmod", "0711", MYCNF_OVERRIDES)
 
     def start_mysql(self, update_db=False):
         LOG.info(_("Starting mysql..."))

pdmars/trove

trove/common/cfg.py
@@ -187,6 +187,8 @@ common_opts = [
     cfg.StrOpt('guest_config',
                default='$pybasedir/etc/trove/trove-guestagent.conf.sample',
                help="Path to guestagent config file"),
+    cfg.StrOpt('validation_rules',
+               default="/etc/trove/validation-rules.json"),
 ]
 
 

trove/guestagent/manager/mysql_service.py
@@ -32,6 +32,8 @@ UUID = False
 
 TMP_MYCNF = "/tmp/my.cnf.tmp"
 MYSQL_BASE_DIR = "/var/lib/mysql"
+MYCNF_OVERRIDES = "/etc/mysql/conf.d/overrides.cnf"
+MYCNF_OVERRIDES_TMP = "/tmp/overrides.cnf.tmp"
 
 CONF = cfg.CONF
 INCLUDE_MARKER_OPERATORS = {
@@ -651,7 +653,7 @@ class MySqlApp(object):
     def complete_install_or_restart(self):
         self.status.end_install_or_restart()
 
-    def secure(self, config_contents):
+    def secure(self, config_contents, overrides):
         LOG.info(_("Generating admin password..."))
         admin_password = generate_random_password()
 
@@ -662,7 +664,7 @@ class MySqlApp(object):
             self._create_admin_user(client, admin_password)
 
         self.stop_db()
-        self._write_mycnf(admin_password, config_contents)
+        self._write_mycnf(admin_password, config_contents, overrides)
         self.start_mysql()
 
         LOG.info(_("Dbaas secure complete."))
@@ -796,7 +798,7 @@ class MySqlApp(object):
                 if "No such file or directory" not in str(pe):
                     raise
 
-    def _write_mycnf(self, admin_password, config_contents):
+    def _write_mycnf(self, admin_password, config_contents, overrides):
         """
         Install the set of mysql my.cnf templates.
         Update the os_admin user and password to the my.cnf
@@ -818,6 +820,33 @@ class MySqlApp(object):
                                    system.MYSQL_CONFIG)
 
         self.wipe_ib_logfiles()
+
+        # write configuration file overrides
+        if overrides:
+            self._write_config_overrides(overrides)
+
+    def _write_config_overrides(self, overrideValues):
+        LOG.info(_("Writing new temp overrides.cnf file."))
+        overrides = open(MYCNF_OVERRIDES_TMP, 'w')
+
+        # write the header of the overrides file
+        overrides.write("[mysqld]\n")
+
+        # overrides that have empty values should be expressed as-is and
+        # with no equals + value parameter.
+        for k, v in overrideValues.iteritems():
+            if not v:
+                overrides.write("%s\n" % k)
+            else:
+                overrides.write("%s = %s\n" % (k, v))
+
+        overrides.close()
+        LOG.info(_("Moving overrides.cnf into correct location."))
+        utils.execute_with_timeout("sudo", "mv", MYCNF_OVERRIDES_TMP,
+                                   MYCNF_OVERRIDES)
+
+        LOG.info(_("Setting permissions on overrides.cnf"))
+        utils.execute_with_timeout("sudo", "chmod", "0711", MYCNF_OVERRIDES)
 
     def start_mysql(self, update_db=False):
         LOG.info(_("Starting mysql..."))